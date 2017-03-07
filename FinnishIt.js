c10d9dd9e3b684a4086f27148db906c118e174aa
MatiasMikkola12 <matu.mikkola@gmail.com>
2017-03-07 00:16:58 -0700

    Update FinnishIt.js
    add comments

--- FinnishIt.js
+++ FinnishIt.js
@@ -3,12 +3,13 @@ import './FinnishIt.css';
 
 class FinnishIt extends Component {
 
+  // adding the super(props) because cant acceess this.props without it in the constructor
   constructor(props) {
     super(props);
     this.state = { greeting: 'Hello' };
     this.finnish = this.finnish.bind(this);
 }
-
+  // pull the greeting (default is english) and name, and allow to call finnish function to switch up the greeting language
 render() {
   return (
     <div className="FinnishIt">
@@ -19,6 +20,7 @@ render() {
   );
 }
 
+  // function for switching up the greeting to finnish
 finnish() {
   this.setState({ greeting: 'Terve, ' });
 }
